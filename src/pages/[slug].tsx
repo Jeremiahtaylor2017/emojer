0f299263fc21fc9d7d8001d33059717829e5679d
jeremiahtaylor2021 <jeremiahtaylor2021@gmail.com>
2023-07-12 00:00:30 -0700

    fixed profile when logged in with google

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -25,9 +25,9 @@ const ProfileFeed = (props: { userId: string }) => {
   )
 }
 
-const ProfilePage: NextPage<{ userId: string }> = ({ userId }) => {
+const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
   const { data } = api.profile.getUserByUsername.useQuery({
-    userId
+    username
   })
 
   console.log(data);
@@ -43,14 +43,14 @@ const ProfilePage: NextPage<{ userId: string }> = ({ userId }) => {
         <div className="h-36 bg-slate-600 relative">
           <Image
             src={data.profileImageUrl}
-            alt={`${data.username}'s profile picture`}
+            alt={`${data.username ?? data.externalUsername}'s profile picture`}
             width={128}
             height={128}
             className="-mb-[64px] absolute bottom-0 left-0 ml-4 rounded-full border-4 border-black bg-black"
           />
         </div>
         <div className="h-[64px]" />
-        <div className="p-4 text-2xl font-bold">{`@${data.username}`}</div>
+        <div className="p-4 text-2xl font-bold">{`@${data.username ?? data.externalUsername}`}</div>
         <div className="w-full border-b border-slate-400" />
         <ProfileFeed userId={data.id} />
       </PageLayout>
